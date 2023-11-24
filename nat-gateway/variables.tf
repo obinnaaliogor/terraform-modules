2d28da521deb72de05a6ea1b39300b191dfe4660
obinnaaliogor <wiz.obi7509@gmail.com>
2023-11-24 01:41:17 +0100

    nat gateway module created

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,12 @@
+# Natgateway Variables
+variable "project_name" {}
+variable "environment" {}
+
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "internet_gateway" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
