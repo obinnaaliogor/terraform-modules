322728f47d230eacdcd498a706a6710242c4a8c1
obinnaaliogor <wiz.obi7509@gmail.com>
2023-11-24 13:32:55 +0100

    alb module created

--- alb/main.tf
+++ alb/main.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "application_load_balancer" {
   internal                   = false
   load_balancer_type         = "application"
   security_groups            = [var.alb_security_group_id]
-  subnets                    = [var.var.public_subnet_az1_id, var.var.public_subnet_az2_id]
+  subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
   enable_deletion_protection = false
 
   tags = {
