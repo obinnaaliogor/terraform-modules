858b39452d604948abecfbdf90ad0291ccc467c7
obinnaaliogor <wiz.obi7509@gmail.com>
2023-11-24 16:54:51 +0100

    update IAM policy

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -61,5 +61,5 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # attach ecs task execution policy to the iam role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = data.aws_iam_policy_document.ecs_task_execution_policy_document.arn
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
