964b3f42e7ad7b7424359fe00a4e8dea56ea291d
obinnaaliogor <wiz.obi7509@gmail.com>
2023-11-24 03:29:53 +0100

    rds module created

---
+++ rds/variables.tf
@@ -0,0 +1,14 @@
+#RDS Variables
+
+variable "project_name" {}
+variable "environment" {}
+
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "db_snapshot_identifier" {}
+variable "instance_class" {}
+variable "multi_az" {}
+variable "database_security_group_id" {}
+variable "identifier" {}
+variable "snapshot_identifier" {}
+variable "availability_zone_1" {}
